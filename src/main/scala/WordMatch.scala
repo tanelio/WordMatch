6ed2c6c4ce1f3b6a71fc5440bbb03b1f68ed0cda
Taneli Otala <taneli@otala.com>
2020-02-08 15:42:27 -0800

    Optimizing a bit

--- src/main/scala/WordMatch.scala
+++ src/main/scala/WordMatch.scala
@@ -22,8 +22,7 @@ object WordMatch extends App {
     codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
     val bufferedSource = Source.fromFile(adj)
     val lineI = bufferedSource.getLines.filter(p => p.head.isDigit)
-    var words: mutable.HashMap[HashSet[Char], (String, String)] = mutable.HashMap.empty
-    var i = 1
+    var words: mutable.HashMap[String, (HashSet[Char], String)] = mutable.HashMap()
     var d = 0
     do { // Skip pre-amble
       val l = lineI.next.split('|')
@@ -31,18 +30,14 @@ object WordMatch extends App {
       if (w(4).contains('-') || w(4).contains(' ') || w(4).contains('_') || w(4).contains('.') || w(4).contains('(')) {
         println(s"Disqualifying: ${w(0)} ${w(4)}")
         d += 1
-        val hs = HashSet() ++ w(4).toArray.toSet
-        words.addOne(hs, (w(4), l(1).tail))
       } else {
-
-        if (i < 10)
-          println(s"$i : id=${w(0)}, word=${w(4)}, expl=${l(1).tail}")
-        else
-          print(s"$i\r")
+        val hs = HashSet() ++ w(4).toArray.toSet
+        words.addOne(w(4), (hs, (l(1).tail)))
+        if (words.size < 10)
+          println(s"id=${w(0)}, word=${w(4)}, expl=${l(1).tail}")
       }
-      i += 1
     } while (lineI.hasNext)
-    println(s"Disqualified = $d, words = $i")
+    println(s"Disqualified = $d, words = ${words.size}")
 
     bufferedSource.close
 
