3b8d10dbc99cd0ac0dc3ed7032ca2b959342b0a3
Taneli Otala <taneli@otala.com>
2020-02-08 21:05:28 -0800

    Adding all dict's

--- src/main/scala/WordMatch.scala
+++ src/main/scala/WordMatch.scala
@@ -10,12 +10,9 @@ import scala.util.control.Breaks._
 
 object WordMatch extends App {
   var words: mutable.HashMap[String, (HashSet[Char], String)] = mutable.HashMap()
-  var d = 0
-  Initialize("adj")
-  Initialize("adv")
-  Initialize("noun")
-  Initialize("verb")
-  println(s"Disqualified = $d, words = ${words.size}")
+  var discq = 0
+  for (dict <- List("adj", "adv", "noun", "verb")) Initialize(dict)
+  println(s"Disqualified = $discq, words = ${words.size}")
 
   def Initialize(what: String): Unit = {
     println(s"reading $what")
@@ -29,20 +26,20 @@ object WordMatch extends App {
       val l = lineI.next.split('|')
       val c = l(0).split(' ').drop(4)
       breakable {
-        for (wd <- c /*if wd.head.isLetter*/) {
+        for (wd <- c) {
           if (!wd.head.isLetter || wd == "n" || wd == "a") break
           if (wd.contains('.')) {
             println(s"Disqualifying: $wd")
-            d += 1
+            discq += 1
           } else {
             if (wd.contains('(') || wd.contains('-') || wd.contains('_'))  {
               var w = wd
               while (w.contains('(')) w = w.init
               if (w.contains('-'))    w.replaceAllLiterally("-", "")
               if (w.contains('_'))    w.replaceAllLiterally("_", "")
-              words.addOne(wd, (HashSet() ++ w.toArray.toSet, (l(1).tail)))
+              words.addOne(wd, (HashSet() ++ w.toArray.toSet, l(1).tail))
             } else
-              words.addOne(wd, (HashSet() ++ wd.toArray.toSet, (l(1).tail)))
+              words.addOne(wd, (HashSet() ++ wd.toArray.toSet, l(1).tail))
             if (words.size < 40) println(s"word=$wd, expl=${l(1).tail}")
           }
         }
